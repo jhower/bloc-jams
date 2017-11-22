1dfbf6bcfe217aa8f1bf923a0ce2cbd3d50921db
Jordan Hower <jhower91@gmail.com>
2017-11-21 22:19:21 -0700

    Checkpoint-19

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21' },
+         { title: 'Magenta', duration: '2:15' }
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+ };
+
+ var albumKiss = {
+   title: 'Destroyer',
+   artist: 'Kiss',
+   label: 'EM',
+   year: '1909',
+   albumArtUrl: 'assets/images/album_covers/KissAlbumCover.jpg',
+   songs: [
+     { title: 'Detroit Rock City', duration: '5:17' },
+     { title: 'Shout It Out Loud', duration: '2:49' },
+     { title: 'Beth', duration: '2:45'},
+     { title: 'God of Thunder', duration: '4:13' },
+     { title: 'Great Expectations', duration: '4:24'}
+   ]
+ };
