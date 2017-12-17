359506d7ec67a3ad38137bf103ebff02f6f824b3
Jordan Hower <jhower91@gmail.com>
2017-12-17 13:37:41 -0700

    checkpoint-25

--- scripts/album.js
+++ scripts/album.js
@@ -12,6 +12,12 @@ var setSong = function(songNumber) {
 setVolume(currentVolume);
 };
 
+var seek = function(time) {
+  if (currentSoundFile) {
+    currentSoundFile.setTime(time);
+  }
+}
+
 var setVolume = function(volume) {
   if (currentSoundFile) {
     currentSoundFile.setVolume(volume);
@@ -47,6 +53,10 @@ if (currentlyPlayingSongNumber !== null) {
 if (currentlyPlayingSongNumber !== songNumber) {
   setSong(songNumber);
   currentSoundFile.play();
+  var $volumeFill = $('.volume .fill');
+   var $volumeThumb = $('.volume .thumb');
+   $volumeFill.width(currentVolume + '%');
+   $volumeThumb.css({left: currentVolume + '%'});
   $(this).html(pauseButtonTemplate);
   currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
   updatePlayerBarSong();
@@ -63,7 +73,6 @@ if (currentlyPlayingSongNumber !== songNumber) {
   }
 };
 
-
 var onHover = function(event) {
   var songNumberCell = $(this).find('.song-item-number');
   var songNumber = songNumberCell.attr('data-song-number');
@@ -71,6 +80,7 @@ var onHover = function(event) {
       songNumberCell.html(playButtonTemplate);
     }
 };
+
 var offHover = function(event) {
   var songNumberCell = $(this).find('.song-item-number');
   var songNumber = songNumberCell.attr('data-song-number');
@@ -82,7 +92,6 @@ var offHover = function(event) {
   $row.hover(onHover, offHover);
     return $row;
 };
-
 
 var setCurrentAlbum = function(album) {
   currentAlbum = album;
@@ -103,6 +112,72 @@ var setCurrentAlbum = function(album) {
     var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     $albumSongList.append($newRow);
   }
+};
+
+var updateSeekBarWhileSongPlays = function() {
+  if (currentSoundFile) {
+// #10
+    currentSoundFile.bind('timeupdate', function(event) {
+// #11
+      var seekBarFillRatio = this.getTime() / this.getDuration();
+      var $seekBar = $('.seek-control .seek-bar');
+
+      updateSeekPercentage($seekBar, seekBarFillRatio);
+    });
+  }
+};
+
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+  var offsetXPercent = seekBarFillRatio * 100;
+  // #1
+  offsetXPercent = Math.max(0, offsetXPercent);
+  offsetXPercent = Math.min(100, offsetXPercent);
+// #2
+  var percentageString = offsetXPercent + '%';
+  $seekBar.find('.fill').width(percentageString);
+  $seekBar.find('.thumb').css({left: percentageString});
+ };
+
+var setupSeekBars = function() {
+  var $seekBars = $('.player-bar .seek-bar');
+
+  $seekBars.click(function(event) {
+    var offsetX = event.pageX - $(this).offset().left;
+    var barWidth = $(this).width();
+    var seekBarFillRatio = offsetX / barWidth;
+
+    if ($(this).parent().attr('class') == 'seek-control') {
+      seek(seekBarFillRatio * currentSoundFile.getDuration());
+    } else {
+      setVolume(seekBarFillRatio * 100);
+    }
+
+    updateSeekPercentage($(this), seekBarFillRatio);
+  });
+
+  $seekBars.find('.thumb').mousedown(function(event) {
+
+    var $seekBar = $(this).parent();
+
+    $(document).bind('mousemove.thumb', function(event){
+      var offsetX = event.pageX - $seekBar.offset().left;
+      var barWidth = $seekBar.width();
+      var seekBarFillRatio = offsetX / barWidth;
+
+      if ($seekBar.parent().attr('class') == 'seek-control') {
+        seek(seekBarFillRatio * currentSoundFile.getDuration());
+      } else {
+        setVolume(seekBarFillRatio);
+      }
+
+      updateSeekPercentage($seekBar, seekBarFillRatio);
+    });
+
+    $(document).bind('mouseup.thumb', function() {
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+  });
 };
 
 var trackIndex = function(album, song) {
@@ -204,6 +279,7 @@ var togglePlayFromPlayerBar = function() {
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
+  setupSeekBars();
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
   $playerBarPlayButton.click(togglePlayFromPlayerBar);
